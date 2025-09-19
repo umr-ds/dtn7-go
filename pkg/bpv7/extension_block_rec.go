f00aef0eef8882c7f5175f4434288e00f9d6882b
Markus Sommer <msommer@informatik.uni-marburg.de>
2025-09-19 16:08:49 +0200

    Store & load data via DTN

--- pkg/bpv7/extension_block_rec.go
+++ pkg/bpv7/extension_block_rec.go
@@ -16,33 +16,33 @@ const (
 	NTypeClient    RECNodeType = 4
 )
 
-type RECJobQuery struct {
+type RECJobQueryBlock struct {
 	Submitter string
 }
 
-func NewRECJobQueryBlock(Submitter string) *RECJobQuery {
-	query := RECJobQuery{Submitter: Submitter}
+func NewRECJobQueryBlock(Submitter string) *RECJobQueryBlock {
+	query := RECJobQueryBlock{Submitter: Submitter}
 	return &query
 }
 
-func (block *RECJobQuery) CheckValid() error {
+func (block *RECJobQueryBlock) CheckValid() error {
 	return nil
 }
 
-func (block *RECJobQuery) BlockTypeCode() uint64 {
+func (block *RECJobQueryBlock) BlockTypeCode() uint64 {
 	return BlockTypeRECJobQuery
 }
 
-func (block *RECJobQuery) BlockTypeName() string {
+func (block *RECJobQueryBlock) BlockTypeName() string {
 	return "REC Job Query"
 }
 
-func (block *RECJobQuery) CheckContextValid(*Bundle) error {
+func (block *RECJobQueryBlock) CheckContextValid(*Bundle) error {
 	// TODO: check if multiple rec blocks exist
 	return nil
 }
 
-func (block *RECJobQuery) MarshalCbor(w io.Writer) error {
+func (block *RECJobQueryBlock) MarshalCbor(w io.Writer) error {
 	if err := cboring.WriteArrayLength(1, w); err != nil {
 		return err
 	}
@@ -54,7 +54,7 @@ func (block *RECJobQuery) MarshalCbor(w io.Writer) error {
 	return nil
 }
 
-func (block *RECJobQuery) UnmarshalCbor(r io.Reader) error {
+func (block *RECJobQueryBlock) UnmarshalCbor(r io.Reader) error {
 	if l, err := cboring.ReadArrayLength(r); err != nil {
 		return err
 	} else if l != 1 {
@@ -67,6 +67,83 @@ func (block *RECJobQuery) UnmarshalCbor(r io.Reader) error {
 	}
 
 	block.Submitter = submitter
+
+	return nil
+}
+
+type RECNamedDataAction uint64
+
+const (
+	RECNamedDataActionPut    RECNamedDataAction = 1
+	RECNamedDataActionGet    RECNamedDataAction = 2
+	RECNamedDataActionDelete RECNamedDataAction = 3
+)
+
+type RECNamedDataBlock struct {
+	Action RECNamedDataAction
+	Name   string
+}
+
+func NewRECNamedDataBlock(action RECNamedDataAction, name string) *RECNamedDataBlock {
+	block := RECNamedDataBlock{
+		Action: action,
+		Name:   name,
+	}
+	return &block
+}
+
+func (block *RECNamedDataBlock) CheckValid() error {
+	return nil
+}
+
+func (block *RECNamedDataBlock) BlockTypeCode() uint64 {
+	return BlockTypeRECNamedData
+}
+
+func (block *RECNamedDataBlock) BlockTypeName() string {
+	return "REC Named Data"
+}
+
+func (block *RECNamedDataBlock) CheckContextValid(*Bundle) error {
+	// TODO: check if multiple rec blocks exist
+	return nil
+}
+
+func (block *RECNamedDataBlock) MarshalCbor(w io.Writer) error {
+	if err := cboring.WriteArrayLength(2, w); err != nil {
+		return err
+	}
+
+	if err := cboring.WriteUInt(uint64(block.Action), w); err != nil {
+		return err
+	}
+
+	if err := cboring.WriteTextString(block.Name, w); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (block *RECNamedDataBlock) UnmarshalCbor(r io.Reader) error {
+	if l, err := cboring.ReadArrayLength(r); err != nil {
+		return err
+	} else if l != 2 {
+		return fmt.Errorf("expected 2 field, got %d", l)
+	}
+
+	action, err := cboring.ReadUInt(r)
+	if err != nil {
+		return err
+	}
+	block.Action = RECNamedDataAction(action)
+
+	name, err := cboring.ReadTextString(r)
+	if err != nil {
+		return err
+	}
+
+	block.Name = name
 
 	return nil
 }
