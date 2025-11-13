92c7240e44cf2122ffdd7e6943c583d67505d292
Markus Sommer <msommer@informatik.uni-marburg.de>
2025-11-13 15:47:31 +0100

    Synchronize operations on BundleDescriptor

--- pkg/store/bundle_descriptor.go
+++ pkg/store/bundle_descriptor.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"sync"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -37,23 +38,27 @@ type BundleMetadata struct {
 // while the other fields contain data that doesn't make sense to persist.
 type BundleDescriptor struct {
 	// Metadata is all data which should be preserved to the database
-	Metadata BundleMetadata
+	Metadata   BundleMetadata
+	stateMutex sync.RWMutex
 
 	// RetentionConstraints as defined by RFC9171 Section 5, see constraints.go for possible values
 	RetentionConstraints []Constraint
 	// should this bundle be retained, i.e. protected from deletion
 	// bundle's with constraints are also currently being processed
-	Retain bool
+	retain bool
 	// should this bundle be dispatched?
-	Dispatch bool
+	dispatch bool
+	// has this bundle been deleted
+	deleted bool
 }
 
 func NewBundleDescriptor(metadata BundleMetadata) *BundleDescriptor {
 	bd := BundleDescriptor{
 		Metadata:             metadata,
 		RetentionConstraints: []Constraint{DispatchPending},
-		Retain:               true,
-		Dispatch:             true,
+		retain:               true,
+		dispatch:             true,
+		deleted:              false,
 	}
 	return &bd
 }
@@ -61,16 +66,25 @@ func NewBundleDescriptor(metadata BundleMetadata) *BundleDescriptor {
 // Load loads the entire bundle from disk
 // Since bundles can be rather arbitrarily large, this can be very expensive and should only be done when necessary.
 func (bd *BundleDescriptor) Load() (*bpv7.Bundle, error) {
+	bd.stateMutex.RLock()
+	defer bd.stateMutex.RUnlock()
+
 	return GetStoreSingleton().loadEntireBundle(bd.Metadata.SerialisedFileName)
 }
 
 // GetKnownHolders gets the list of EndpointIDs which we know to already have received the bundle.
 func (bd *BundleDescriptor) GetKnownHolders() []bpv7.EndpointID {
+	bd.stateMutex.RLock()
+	defer bd.stateMutex.RUnlock()
+
 	return bd.Metadata.KnownHolders
 }
 
 // AddKnownHolder adds EndpointIDs to this bundle's list of known recipients.
 func (bd *BundleDescriptor) AddKnownHolder(peers ...bpv7.EndpointID) {
+	bd.stateMutex.Lock()
+	defer bd.stateMutex.Unlock()
+
 	bd.Metadata.KnownHolders = append(bd.Metadata.KnownHolders, peers...)
 	err := GetStoreSingleton().updateBundleMetadata(bd.Metadata)
 	if err != nil {
@@ -88,34 +102,85 @@ func (bd *BundleDescriptor) AddKnownHolder(peers ...bpv7.EndpointID) {
 
 // AddConstraint adds a Constraint to this bundle and checks if it should be retained/dispatched.
 func (bd *BundleDescriptor) AddConstraint(constraint Constraint) error {
+	bd.stateMutex.Lock()
+	defer bd.stateMutex.Unlock()
+
 	if !constraint.Valid() {
 		return NewInvalidConstraint(constraint)
 	}
 
 	bd.RetentionConstraints = append(bd.RetentionConstraints, constraint)
-	bd.Retain = true
-	bd.Dispatch = constraint != ForwardPending
+
+	// if there's at least one constraint, the bundle should be retained
+	bd.retain = true
+
+	// if the bundle has the ForwardPending constraint, then it is currently going though the processing pipeline
+	// and should not be dispatched again
+	if constraint == ForwardPending {
+		bd.dispatch = false
+	}
 	return nil
 }
 
 // RemoveConstraint removes a Constraint from this bundle and checks if it should be retained/dispatched.
 func (bd *BundleDescriptor) RemoveConstraint(constraint Constraint) {
+	bd.stateMutex.Lock()
+	defer bd.stateMutex.Unlock()
+
+	bd.dispatch = true
 	constraints := make([]Constraint, 0, len(bd.RetentionConstraints))
 	for _, existingConstraint := range bd.RetentionConstraints {
 		if existingConstraint != constraint {
 			constraints = append(constraints, existingConstraint)
+
+			// if the bundle has the ForwardPending constraint, then it is currently going though the processing pipeline
+			// and should not be dispatched again
+			if existingConstraint == ForwardPending {
+				bd.dispatch = false
+			}
 		}
 	}
 	bd.RetentionConstraints = constraints
-	bd.Retain = len(bd.RetentionConstraints) > 0
-	bd.Dispatch = constraint == ForwardPending
+	bd.retain = len(bd.RetentionConstraints) > 0
 }
 
 // ResetConstraints removes all Constraints from this bundle.
 func (bd *BundleDescriptor) ResetConstraints() {
+	bd.stateMutex.Lock()
+	defer bd.stateMutex.Unlock()
+
 	bd.RetentionConstraints = make([]Constraint, 0)
-	bd.Retain = false
-	bd.Dispatch = true
+	bd.retain = false
+	bd.dispatch = true
+}
+
+func (bd *BundleDescriptor) Dispatch() bool {
+	bd.stateMutex.RLock()
+	defer bd.stateMutex.RUnlock()
+
+	return bd.dispatch
+}
+
+func (bd *BundleDescriptor) Retain() bool {
+	bd.stateMutex.RLock()
+	defer bd.stateMutex.RUnlock()
+
+	return bd.retain
+}
+
+func (bd *BundleDescriptor) Deleted() bool {
+	bd.stateMutex.RLock()
+	defer bd.stateMutex.RUnlock()
+
+	return bd.deleted
+}
+
+func (bd *BundleDescriptor) Delete() error {
+	bd.stateMutex.Lock()
+	defer bd.stateMutex.Unlock()
+
+	bd.deleted = true
+	return GetStoreSingleton().DeleteBundle(bd.Metadata)
 }
 
 func (bd *BundleDescriptor) String() string {
